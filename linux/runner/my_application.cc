dc9bc0802da2c576c4aef66161a0866f69623fad
marky <markism1224amarille@gmail.com>
2026-02-27 11:20:08 +0800

    project rename for midterms

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "advmobprog_prelims_tp02_amarille");
+    gtk_header_bar_set_title(header_bar, "advmobprog_midterms_tp02_amarille");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "advmobprog_prelims_tp02_amarille");
+    gtk_window_set_title(window, "advmobprog_midterms_tp02_amarille");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"advmobprog_prelims_tp02_amarille", origin, size)) {
+  if (!window.Create(L"advmobprog_midterms_tp02_amarille", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
